20b771ad99aa32147920f3a404489ece23053e00
Mark Joseph Pizarra <markjosephpizarra@gmail.com>
2025-10-23 23:06:00 +0800

    Add files via upload

---
+++ pizarra-shop/src/test/java/com/pizarrashop/pizarra_shop/PizarraShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizarrashop.pizarra_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizarraShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
